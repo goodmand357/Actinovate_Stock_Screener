952724bf10822fe265f8a1d17f4a8ac483bfc707
Danisha <goodmand@montclair.edu>
2025-04-15 15:32:43 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,4 @@ def ping():
     return "Backend is alive"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
