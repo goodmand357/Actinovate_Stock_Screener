0afc9cedddc03f84cf17f143b6477ff9bf5782fc
Danisha <goodmand@montclair.edu>
2025-04-28 18:47:10 -0400

    Create SomePage.tsx

---
+++ actinovate-frontend-main/src/components/SomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Stocks from "./Stocks"; // your existing Stocks component
+
+const SomePage = () => {
+  return (
+    <div className="p-6">
+      <Stocks />
+    </div>
+  );
+};
+
+export default SomePage;
